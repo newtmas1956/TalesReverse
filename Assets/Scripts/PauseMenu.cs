85976549637481f716e0cbf2fd0531542ae38edf
KsenyaVasilieva2001 <ksenya_vasilieva@mail.ru>
2022-05-16 11:37:55 +0300

    Add main menu, settings, puzzle

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool pauseGame;
+    public GameObject pauseGameMenu;
+
+   /* void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    */
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            if (pauseGame)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        pauseGameMenu.SetActive(false);
+        Time.timeScale = 1f;
+        pauseGame = false;
+    }
+
+    public void Pause()
+    {
+        pauseGameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        pauseGame = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMP_Dropdown dropdown;
+    Resolution[] resolutions;
+    List<string> resolutionsList;
+    public bool isFullScreen;
+
+    public void Awake()
+    {
+        resolutionsList = new List<string>();
+        resolutions = Screen.resolutions;
+        foreach (var i in resolutions)
+        {
+            resolutionsList.Add(i.width +":" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutionsList);
+        isFullScreen = Screen.fullScreen;
+    }
+    
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    public void FullScreen()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+    
+    
+}
+
